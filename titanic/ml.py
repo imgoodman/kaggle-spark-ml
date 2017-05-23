069e8fd10a9eacbcc83822f098ba8ab1f20447df
imgoodman <jan8611@163.com>
2017-05-23 22:04:05 +0800

    train.csv in data

--- titanic/ml.py
+++ titanic/ml.py
@@ -2,6 +2,11 @@ from pyspark import SparkContext
 
 sc=SparkContext("local[2]","titanic spark app")
 
-input_file="input/train.csv"
+input_file="/usr/bigdata/spark/kaggle-spark-ml-app/kaggle-spark-ml/titanic/data/train.csv"
 
 raw_data=sc.textFile(input_file)
+
+#raw_records=raw_data.map(lambda line: line.split(",")).map(lambda (passengerId, survived, pclass, name, sex, age, sibsp, parch, ticket, fare, cabin, embarked):(pclass, sex, age, sibsp, parch, ticket, fare, cabin, embarked, survived))
+#raw_records=raw_data.map(lambda line: line.split(",")).map(lambda records:(records[2],records[4], records[5],records[6], records[7], records[8], records[9], records[10], records[11], records[1]))
+raw_records=raw_data.map(lambda line: line.split(","))
+print raw_records.first()
