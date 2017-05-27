a6ddc4b5c6febb2d9b0f12b87d0bb481293980d7
imgoodman <jan8611@163.com>
2017-05-27 21:10:23 +0800

    explore house data

--- houseprice/explore.py
+++ houseprice/explore.py
@@ -12,10 +12,62 @@ raw_data=sc.textFile(train_file_path).map(lambda line:line.split(","))
 """
 total_houses=raw_data.count()
 print "total %d houses" % total_houses
+
+def getMapOfColumn(idx):
+    return raw_data.map(lambda fields:fields[idx]).distinct().zipWithIndex().collectAsMap()
+
+
 """
 msubclass
 the build class
 不知道这个字段是什么意思
+it seems that it is enumerated
 """
 msubclass=raw_data.map(lambda fields:fields[1])
-print msubclass.countByValue()
+#print msubclass.countByValue()
+"""
+try to change to enumerate values
+"""
+msubclass_map=getMapOfColumn(1)
+
+"""
+MSZoning
+it seems that it is enumerated
+can into types
+"""
+mszoning=raw_data.map(lambda fields:fields[2])
+#print mszoning.countByValue()
+mszoning_map=getMapOfColumn(2)
+
+"""
+LotFrontage
+linear feet of street connected to property
+number with NA values
+need to fill NA values
+"""
+
+
+
+"""
+LotArea
+Lot size in square feet
+number
+"""
+
+"""
+street
+tyoe of road access
+enumerate: pave or grvl
+can into type
+"""
+street_map=getMapOfColumn(5)
+
+"""
+alley
+type of alley access
+emnurate: pave, grvl
+value with NA
+"""
+
+"""
+
