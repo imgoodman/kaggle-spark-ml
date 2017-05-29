c29ff88064ebb5c547cb58a5d07418c9bf5a1b00
imgoodman <jan8611@163.com>
2017-05-29 13:30:51 +0800

    explore original data

--- houseprice/ml-original-data.py
+++ houseprice/ml-original-data.py
@@ -5,7 +5,7 @@ import numpy as np
 
 train_file_path="/usr/bigdata/data/houseprice/noheader_train.csv"
 
-sc=SparkContext(train_file_path)
+sc=SparkContext("local[2]","spark kaggle house price regression")
 
 raw_data=sc.textFile(train_file_path).map(lambda line:line.split(","))
 
@@ -18,4 +18,36 @@ number_columns_with_many_zeros=[26,34,36,37,38,44,45,62,66,67,68,69,70,71,75,]
 
 saleprice_column=80
 
+def getMapOfColumn(idx):
+    return raw_data.map(lambda fields:fields[idx]).distinct().zipWithIndex().collectAsMap()
 
+def get_type_maps():
+    type_maps={}
+    for i in type_columns:
+        type_maps[i]=getMapOfColumn(i)
+    return type_maps
+
+type_maps=get_type_maps()
+#print type_maps
+
+def get_type_cnt(maps):
+    return sum([len(maps[i]) for i in maps])
+    
+type_cnt=get_type_cnt(type_maps)
+number_cnt=len(number_columns)
+total=type_cnt+number_cnt
+#print total
+
+def extract_features(fields):
+    features=np.zeros(total)
+    step=0
+    for i in type_columns:
+        features[step+ int(type_maps[i][fields[i]]) ]=1.0
+        step=step+len(type_maps[i])
+    for i in number_columns:
+        features[step]=float(fields[i])
+        step=step+1
+    return features
+
+data=raw_data.map(lambda fields: LabeledPoint(float(fields[saleprice_column]),extract_features(fields)))
+print data.first()
