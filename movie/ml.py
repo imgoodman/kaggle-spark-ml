018e5e7232296e1c1d66051f7955cfe51e461066
imgoodman <jan8611@163.com>
2017-05-31 21:00:30 +0800

    recomment movie for user

--- movie/ml.py
+++ movie/ml.py
@@ -5,13 +5,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 train_file_path="/usr/bigdata/data/ml-100k/u.data"
+movie_file_path="/usr/bigdata/data/ml-100k/u.item"
 
 sc=SparkContext("local[2]","spark movie app")
 
 raw_data=sc.textFile(train_file_path).map(lambda line: line.split("\t"))
 
-#print(raw_data.first())
 
+raw_movies=sc.textFile(movie_file_path).map(lambda line: line.split("|"))
+#print raw_movies.take(10)
+movies=raw_movies.map(lambda fields:(int(fields[0]), fields[1]))
+#print movies.take(10)
+def getMovieById(movieId):
+    return movies.filter(lambda (id,title):id==movieId).collectAsMap()
+#print(raw_data.first())
+#print(" movie is:" +  str(getMovieById(123)))
 
 """
 评级矩阵是943*1682
@@ -52,5 +60,45 @@ model=ALS.train(ratings, rank, iterations, regParam)
 #print(model.useurFeatures)
 #print(model.productFeatures)
 
-predRating=model.predict(789, 123)
-print(predRating)
+#predRating=model.predict(789, 123)
+#print(predRating)
+
+
+"""
+为某个用户推荐K个物品
+recommendProducts(user, num)
+其中,user：用户ID
+num：要推荐的物品个数
+给指定用户推荐的电影
+"""
+def recommend():
+    userId=789
+    K=10
+    opKRecs=model.recommendProducts(userId, K)
+    print "model recommend movies:"
+    print(topKRecs)
+    """
+    指定用户实际看过的电影
+    """
+    moviesForUser=ratings.filter(lambda r: r[0]==userId)
+    #print moviesForUser.collect()
+    topActualMovies=moviesForUser.map(lambda r: (r.product, r.rating)).map(lambda (k,v): (v,k)).sortByKey(ascending=False)
+    print("actual movies seen:")
+    print topActualMovies.take(10)
+
+
+"""
+物品推荐：给定物品，哪些物品与它相似
+常见的相似度衡量方法：
+1.皮尔森相关系数Pearson correlation
+2.针对实数的余弦相似度cosine similarity，指的是两个向量在n维空间里两者夹角的度数，它是两个向量的点积与各向量范数的乘积的商。取值-1到1，1表示完全相似，0表示两者互不相关，无相似性。-1表示两者不仅不相关，而且完全不相同
+3.针对二元向量的杰卡德相似系数Jaccard similarity
+"""
+def cosine_similarity(vec1, vec2):
+    return vec1.dot(vec2)/(vec1.norm() * vec2.norm())
+def product_similar():
+    from jblas import DoubleMatrix
+    itemId=567
+    itemFactor=model.productFeatures.filter(lambda (id,factor):id==itemId).map(lambda (id,factor): DoubleMatrix(factor))
+    print cosine_similarity(itemFactor, itemFactor)
+product_similar()
